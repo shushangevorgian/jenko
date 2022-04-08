533bcb19de38c489e4ae8e09c534a463eca2fdf3
shushangevorgian <95764164+shushangevorgian@users.noreply.github.com>
2022-04-08 21:56:56 +0400

    Test for code

---
+++ test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import unittest
+import code
+
+class Testing(unittest.TestCase):
+    def test_string(self):
+        a = 'some'
+        b = 'some'
+        self.assertEqual(a, b)
+
+    def test_boolean(self):
+        a = True
+        b = True
+        self.assertEqual(a, b)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
